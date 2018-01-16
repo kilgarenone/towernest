9ffac2534dd390a29dd69f29d97311a9cf5aafb7
Kheoh Yee Wei <kwei88@gmail.com>
2018-01-16 10:53:47 +0800

    add logging to store dispatch

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,30 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { createEpicMiddleware } from "redux-observable";
 import { rootEpic, rootReducer } from "./root";
 
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+  return action => {
+    console.group(action.type);
+    console.log(
+      "%c prev state ",
+      "background: gray; color: white",
+      store.getState()
+    );
+    console.log("%c action ", "background: blue; color: white", action);
+    const returnValue = rawDispatch(action);
+    console.log(
+      "%c next state ",
+      "background: green; color: white",
+      store.getState()
+    );
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const composeEnhancers =
     // eslint-disable-next-line
@@ -13,6 +37,11 @@ const configureStore = () => {
     rootReducer,
     composeEnhancers(applyMiddleware(epicMiddleware))
   );
+
+  if (process.env.NODE_ENV !== "production") {
+    store.dispatch = addLoggingToDispatch(store);
+  }
+
   return store;
 };
 
