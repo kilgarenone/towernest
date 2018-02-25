553411b15f0ade3512e9b22b5249391f4780e5a3
Kheoh Yee Wei <kwei88@gmail.com>
2018-02-25 22:19:44 +0800

    test json server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,35 @@
 // @flow
-import React from "react";
-import {
-  XYPlot,
-  LineSeries,
-  XAxis,
-  YAxis,
-  HorizontalGridLines,
-  VerticalGridLines
-} from "react-vis";
+import React, { Component } from "react";
+// import {
+//   XYPlot,
+//   LineSeries,
+//   XAxis,
+//   YAxis,
+//   HorizontalGridLines
+// } from "react-vis";
+import axios from "axios";
 import { connect } from "react-redux";
 import "../../node_modules/react-vis/dist/style.css";
 import Header from "./Header";
 import Heading from "./Heading";
 import { getSelectedStocksDetails } from "../redux/viewStocks";
 
-const App = ({ stocks = [] }) => (
-  <div>
-    <Header />
-    <main>
-      <Heading tag="h4">Apple</Heading>
-      <XYPlot xType="time" height={600} width={600} yPadding="5">
-        <HorizontalGridLines />
-        <XAxis />
-        <YAxis />
-        <LineSeries data={stocks} />
-      </XYPlot>
-    </main>
-  </div>
-);
+class App extends Component<any, any> {
+  componentWillMount() {
+    axios.get("testApi").then(res => console.log("res", res));
+  }
+  render() {
+    return (
+      // const App = ({ stocks = [] }) => (
+      <div>
+        <Header />
+        <main>
+          <Heading tag="h4">Apple</Heading>
+        </main>
+      </div>
+    );
+  }
+}
 export default connect(
   state => ({ stocks: getSelectedStocksDetails(state) }),
   null
