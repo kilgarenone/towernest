2be88c1086ca05220708aca346ee3003c2acc577
Kheoh Yee Wei <kwei88@gmail.com>
2018-02-26 21:52:34 +0800

    working on oauth

--- src/config.js
+++ src/config.js
@@ -1,7 +1,2 @@
-const API_ENDPOINTS_BASE_URL = "https://api.iextrading.com/1.0/";
-
-export const getStockQuoteBySymbolApi = symbol =>
-  `${API_ENDPOINTS_BASE_URL}/stock/${symbol}/quote`;
-
-export const getChartBySymbolAndRangeApi = (symbol, range) =>
-  `${API_ENDPOINTS_BASE_URL}/stock/${symbol}/chart/${range}`;
+export const CLIENT_ID = "1234";
+export const CLIENT_SECRET = "5678";
