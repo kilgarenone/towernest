d0de6088fd15bf67d3c1ab30485477fcc02ddca6
Kheoh Yee Wei <kwei88@gmail.com>
2018-05-27 11:42:37 +0800

    Removes extra < character

--- src/modules/home/view.jsx
+++ src/modules/home/view.jsx
@@ -52,7 +52,6 @@ class Home extends Component {
         data={this.state.portfolioGrowthTrend}
         margin={{ top: 10, right: 30, left: 30, bottom: 10 }}
       >
-        >
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
