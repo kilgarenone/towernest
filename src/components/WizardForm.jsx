1c91dc4712aff6f0d2577e61b9428750a0ccdd4c
Kheoh Yee Wei <kwei88@gmail.com>
2018-07-15 21:39:44 +0800

    Creates Wizard form component

---
+++ src/components/WizardForm.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Formik, Field } from "formik";
+
+class Wizard extends Component {
+  static Page = ({ children }) => children;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+      values: props.initialValues
+    };
+  }
+
+  next = values =>
+    this.setState(state => ({
+      page: Math.min(state.page + 1, this.props.children.length - 1),
+      values
+    }));
+
+  previous = () =>
+    this.setState(state => ({
+      page: Math.max(state.page - 1, 0)
+    }));
+
+  validate = values => {
+    const activePage = React.Children.toArray(this.props.children)[
+      this.state.page
+    ];
+    return activePage.props.validate ? activePage.props.validate(values) : {};
+  };
+
+  handleSubmit = (values, bag) => {
+    const { children, onSubmit } = this.props;
+    const { page } = this.state;
+    const isLastPage = page === React.Children.count(children) - 1;
+    if (isLastPage) {
+      return onSubmit(values);
+    }
+    this.next(values);
+    bag.setSubmitting(false);
+  };
+
+  render() {
+    const { children } = this.props;
+    const { page, values } = this.state;
+    const activePage = React.Children.toArray(children)[page];
+    const isLastPage = page === React.Children.count(children) - 1;
+    return (
+      <Formik
+        initialValues={values}
+        enableReinitialize={false}
+        validate={this.validate}
+        onSubmit={this.handleSubmit}
+      >
+        ({(values, handleSubmit, isSubmitting, handleReset)}) => (
+        <form onSubmit={handleSubmit}>
+          {activePage}
+          <div className="buttons">
+            {page > 0 && (
+              <button type="button" onClick={this.previous}>
+                « Previous
+              </button>
+            )}
+            {!isLastPage && <button type="submit">Next »</button>}
+            {isLastPage && (
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            )}
+          </div>
+        </form>
+        )}
+      </Formik>
+    );
+  }
+}
+
+export default Wizard;
