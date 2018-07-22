a379566f5c0906891a8a906c3600695249e69300
Kheoh Yee Wei <kwei88@gmail.com>
2018-07-22 15:57:21 +0800

    Update header's color to access 'headerColor' key

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -26,30 +26,30 @@ const styles = {
     font-size: ${fontSize.displayLarge};
     font-weight: ${fontWeight.bold};
     line-height: ${lineHeight.displayLarge};
-    color: ${colors.textOnLightBg};
+    color: ${colors.headerColor};
   `,
   displayMedium: css`
     font-size: ${fontSize.displayMedium};
     font-weight: ${fontWeight.normal};
     line-height: ${lineHeight.displayLarge};
-    color: ${colors.textOnLightBg};
+    color: ${colors.headerColor};
   `,
   displaySmall: css`
     font-size: ${fontSize.displaySmall};
     font-weight: ${fontWeight.bold};
     line-height: ${lineHeight.displaySmall};
-    color: ${colors.textOnLightBg};
+    color: ${colors.headerColor};
   `,
   heading: css`
     font-size: ${fontSize.heading};
     font-weight: ${fontWeight.bold};
     line-height: ${lineHeight.heading};
-    color: ${colors.textOnLightBg};
+    color: ${colors.headerColor};
   `,
   subheading: css`
     font-size: ${fontSize.subheading};
     font-weight: ${fontWeight.bold};
     line-height: ${lineHeight.subheading};
-    color: ${colors.textOnLightBg};
+    color: ${colors.headerColor};
   `
 };
