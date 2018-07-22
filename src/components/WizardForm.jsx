62d585fda69ff09f1a907331850483f2b934af7a
Kheoh Yee Wei <kwei88@gmail.com>
2018-07-22 19:15:10 +0800

    Validate using yup schema and auto proceed if validated

--- src/components/WizardForm.jsx
+++ src/components/WizardForm.jsx
@@ -23,21 +23,33 @@ class Wizard extends Component {
       page: Math.max(state.page - 1, 0)
     }));
 
-  validate = values => {
+  validate = async values => {
     const activePage = React.Children.toArray(this.props.children)[
       this.state.page
     ];
-    return activePage.props.validate ? activePage.props.validate(values) : {};
+
+    try {
+      if (!activePage.props.validationSchema) {
+        return {};
+      }
+      await activePage.props.validationSchema.validate(values);
+      setTimeout(() => this.next(values), 250);
+    } catch (err) {
+      console.log("err", err);
+    }
   };
 
   handleSubmit = (values, bag) => {
     const { children, onSubmit } = this.props;
     const { page } = this.state;
     const isLastPage = page === React.Children.count(children) - 1;
+
     if (isLastPage) {
       return onSubmit(values);
     }
+
     this.next(values);
+
     bag.setSubmitting(false);
   };
 
@@ -46,6 +58,7 @@ class Wizard extends Component {
     const { page, values } = this.state;
     const activePage = React.Children.toArray(children)[page];
     const isLastPage = page === React.Children.count(children) - 1;
+
     return (
       <Formik
         initialValues={values}
@@ -53,23 +66,23 @@ class Wizard extends Component {
         validate={this.validate}
         onSubmit={this.handleSubmit}
       >
-        ({(values, handleSubmit, isSubmitting, handleReset)}) => (
-        <form onSubmit={handleSubmit}>
-          {activePage}
-          <div className="buttons">
-            {page > 0 && (
-              <button type="button" onClick={this.previous}>
-                « Previous
-              </button>
-            )}
-            {!isLastPage && <button type="submit">Next »</button>}
-            {isLastPage && (
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            )}
-          </div>
-        </form>
+        {({ values, handleSubmit, isSubmitting, handleReset }) => (
+          <form onSubmit={handleSubmit}>
+            {activePage}
+            <div className="buttons">
+              {page > 0 && (
+                <button type="button" onClick={this.previous}>
+                  « Previous
+                </button>
+              )}
+              {!isLastPage && <button type="submit">Next »</button>}
+              {isLastPage && (
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              )}
+            </div>
+          </form>
         )}
       </Formik>
     );
