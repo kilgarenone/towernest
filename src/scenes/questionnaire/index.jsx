d8a4329bcfc1870fc34959adaa3175a4ecb284d1
Kheoh Yee Wei <kwei88@gmail.com>
2018-07-29 18:38:42 +0800

    Emulating inspired by typeform behaviour for questionaire

--- src/scenes/questionnaire/index.jsx
+++ src/scenes/questionnaire/index.jsx
@@ -1,19 +1,17 @@
+import { Field } from "formik";
 import React, { Component } from "react";
+import { css, cx } from "react-emotion";
+import ScrollPercentage from "react-scroll-percentage";
+import * as Yup from "yup";
 import Container from "./../../components/Container";
-import ErrorMessage from "./../../components/ErrorMessage";
-import OptionallyDisplayed from "./../../components/OptionallyDisplayed";
 import RadioButton from "./../../components/RadioButton";
+import Wizard from "./../../components/WizardForm";
 import spacing from "./../../styles/base/spacing";
 import { fontSize } from "./../../styles/base/typography";
-import Wizard from "./../../components/WizardForm";
-import { Field } from "formik";
-import * as Yup from "yup";
-import Observer from "react-intersection-observer";
-import { css, cx } from "react-emotion";
 
 const opacity = css`
   opacity: 0.2;
-  transition: opacity 1200ms;
+  transition: opacity 200ms;
 `;
 
 const radioBtn = css`
@@ -26,11 +24,18 @@ function QuestionWithRadioButtons({
   questions,
   inView,
   refRoot,
+  percentage,
   field: { name, value, onChange }
 }) {
-  console.log("inView", inView);
+  console.log("name", name);
+  console.log("inView", percentage);
   return (
-    <div ref={refRoot} className={cx(radioBtn, { [opacity]: !inView })}>
+    <div
+      ref={refRoot}
+      className={cx(radioBtn, {
+        [opacity]: percentage < 0.2 || percentage > 0.6
+      })}
+    >
       <div
         style={{
           marginBottom: spacing.space1,
@@ -63,9 +68,20 @@ function QuestionWithRadioButtons({
 const questions1 = [
   { text: "haha", value: "damn", name: "haha" },
   { text: "haha1", value: "damn2", name: "haha1" },
-  { text: "haha2", value: "damn3", name: "haha2" }
+  { text: "haha2", value: "damn3", name: "haha2" },
+  { text: "haha", value: "damn4", name: "haha3" },
+  { text: "haha1", value: "damn5", name: "haha4" },
+  { text: "haha2", value: "damn6", name: "haha5" },
+  { text: "haha", value: "damn7", name: "haha6" },
+  { text: "haha1", value: "damn8", name: "haha7" },
+  { text: "haha2", value: "damn9", name: "haha9" }
 ];
 
+const questions2 = [
+  { text: "haha", value: "damn", name: "haha" },
+  { text: "haha1", value: "damn2", name: "haha1" },
+  { text: "haha2", value: "damn3", name: "haha2" }
+];
 class Questionnaire extends Component {
   state = {};
 
@@ -91,36 +107,54 @@ class Questionnaire extends Component {
             age: Yup.string().required()
           })}
         >
-          <Observer threshold={0.6}>
-            {({ inView, ref }) => (
+          <ScrollPercentage>
+            {({ percentage, inView }) => (
               <Field
                 inView={inView}
-                refRoot={ref}
+                percentage={percentage}
                 name="age"
                 questionText="what;s up"
                 component={QuestionWithRadioButtons}
                 questions={questions1}
               />
             )}
-          </Observer>
+          </ScrollPercentage>
         </Wizard.Page>
         <Wizard.Page
           validationSchema={Yup.object().shape({
             age: Yup.string().required()
           })}
         >
-          <Observer threshold={0.6}>
-            {({ inView, ref }) => (
+          <ScrollPercentage>
+            {({ percentage, inView }) => (
               <Field
+                percentage={percentage}
                 inView={inView}
-                refRoot={ref}
+                name="age2"
+                questionText="what;s up"
+                component={QuestionWithRadioButtons}
+                questions={questions2}
+              />
+            )}
+          </ScrollPercentage>
+        </Wizard.Page>
+        <Wizard.Page
+          validationSchema={Yup.object().shape({
+            age: Yup.string().required()
+          })}
+        >
+          <ScrollPercentage>
+            {({ percentage, inView }) => (
+              <Field
+                percentage={percentage}
+                inView={inView}
                 name="age1"
                 questionText="what;s up"
                 component={QuestionWithRadioButtons}
                 questions={questions1}
               />
             )}
-          </Observer>
+          </ScrollPercentage>
         </Wizard.Page>
       </Wizard>
     );
