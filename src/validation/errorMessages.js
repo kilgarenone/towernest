a5cc295c6b0ec3b955ad9228671706e97fca4b11
Kheoh Yee Wei <kwei88@gmail.com>
2018-08-04 16:00:05 +0800

    Removes manual form validation wirings

--- src/validation/errorMessages.js
+++
@@ -1,7 +0,0 @@
-export const isRequired = fieldName => `${fieldName} is required`;
-
-export const mustMatch = otherFieldName => fieldName =>
-  `${fieldName} must match ${otherFieldName}`;
-
-export const minLength = length => fieldName =>
-  `${fieldName} must be at least ${length} characters`;
