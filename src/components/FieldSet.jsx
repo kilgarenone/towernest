d90a421c99214ce7bdc35ec74d0e8aef56a8dc95
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-10 12:08:05 +0800

    Set border to 1px on fieldset

--- src/components/FieldSet.jsx
+++ src/components/FieldSet.jsx
@@ -3,7 +3,7 @@ import styled from "react-emotion";
 import { fontSize } from "../styles/base/typography";
 
 const FieldSetBase = styled("fieldset")`
-  border: 2px solid #9e9e9e;
+  border: 1px solid #9e9e9e;
 `;
 
 const Legend = styled("legend")`
