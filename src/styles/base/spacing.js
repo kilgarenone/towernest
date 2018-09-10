774afe2e99a7bf30498e9960d3ce56a16b2a2362
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-10 15:42:18 +0800

    Creates L size of 'space4'

--- src/styles/base/spacing.js
+++ src/styles/base/spacing.js
@@ -19,6 +19,7 @@ const spacing: Spacing = {
   space2: `${computeGoldenRatio(spacingFactor, 2)}em`, // 21
   space3: `${computeGoldenRatio(spacingFactor, 3)}em`, // 34
   space4: `${computeGoldenRatio(spacingFactor, 4)}em`, // 55
+  space4L: `${computeGoldenRatio(spacingFactor, 4.75)}em`, // 55
   space5: `${computeGoldenRatio(spacingFactor, 5)}em` // 89
 };
 
