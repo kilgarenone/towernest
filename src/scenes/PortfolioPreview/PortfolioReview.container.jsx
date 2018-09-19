249b817eeb4d9c562fdb734fd3660a78639c94f0
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-19 20:37:15 +0800

    Creating portfolio holdings breakdown chart

--- src/scenes/PortfolioPreview/PortfolioReview.container.jsx
+++ src/scenes/PortfolioPreview/PortfolioReview.container.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { css } from "react-emotion";
 import Heading from "../../components/Heading";
 import Container from "../../components/Container";
 import Button from "../../components/Button";
@@ -13,8 +14,8 @@ class PortfolioReview extends Component {
     const data = this.props.location.state;
 
     return (
-      <div>
-        <Heading tag="h3">Your Portfolio</Heading>
+      <div style={{ paddingTop: "50px" }}>
+        {/* <Heading tag="h3">Your Portfolio</Heading> */}
         <Container isColumn xAlign="center">
           <div style={{ maxWidth: "23em", paddingBottom: spacing.space1 }}>
             Based on your answers, we recommend the
@@ -29,6 +30,30 @@ class PortfolioReview extends Component {
             {data.portfolio.name}
           </div>
           <Button>Open My Account</Button>
+        </Container>
+        <Container
+          xAlign="space-around"
+          className={css`
+            min-height: 400px;
+          `}
+        >
+          {data.holdings.map(holding => (
+            <div style={{ flexBasis: "20%" }}>
+              <div>{holding.assetClass}</div>
+              <div>{`${holding.weight}%`}</div>
+              <div style={{ position: "relative", height: "100%" }}>
+                <div
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    width: "100%",
+                    height: `${holding.weight * 2.5}%`,
+                    backgroundColor: "red"
+                  }}
+                />
+              </div>
+            </div>
+          ))}
         </Container>
       </div>
     );
