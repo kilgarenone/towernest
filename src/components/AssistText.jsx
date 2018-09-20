5dd2b4ec4d17ba244db869033415babd7fa69d63
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-20 10:43:49 +0800

    Creates <AssistText />

---
+++ src/components/AssistText.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { css, cx } from "react-emotion";
+import { fontSize } from "../styles/typography";
+
+const defaultCss = css`
+  font-size: ${fontSize.subText};
+  font-weight: 500;
+  align-self: flex-end;
+  color: rgba(0, 0, 0, 0.44);
+  cursor: pointer;
+
+  &:focus,
+  &:hover {
+    color: rgba(0, 0, 0, 0.64);
+    text-decoration: underline;
+  }
+
+  &.underline {
+    text-decoration: underline;
+  }
+`;
+
+function AssistText({ hasUnderline = false, children, className, ...props }) {
+  return (
+    <div
+      {...props}
+      className={cx(defaultCss, className, { underline: hasUnderline })}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default AssistText;
