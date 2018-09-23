ddbcbabe8470ae1b9516ef893a9968c13101db5d
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-23 12:48:10 +0800

    Create utilities classes css

---
+++ src/styles/utilities.js
@@ -0,0 +1,64 @@
+/* eslint-disable import/prefer-default-export  */
+
+import { css } from "react-emotion";
+import spacing from "./spacing";
+
+export const marginBottom0 = css`
+  margin-bottom: ${spacing.space0};
+`;
+export const marginBottom1 = css`
+  margin-bottom: ${spacing.space1};
+`;
+export const marginBottom2 = css`
+  margin-bottom: ${spacing.space2};
+`;
+export const marginBottom3 = css`
+  margin-bottom: ${spacing.space3};
+`;
+export const marginBottom4 = css`
+  margin-bottom: ${spacing.space4};
+`;
+export const marginBottom5 = css`
+  margin-bottom: ${spacing.space5};
+`;
+
+export const paddingBottom0 = css`
+  padding-bottom: ${spacing.space0};
+`;
+export const paddingBottom1 = css`
+  padding-bottom: ${spacing.space1};
+`;
+export const paddingBottom2 = css`
+  padding-bottom: ${spacing.space2};
+`;
+export const paddingBottom3 = css`
+  padding-bottom: ${spacing.space3};
+`;
+export const paddingBottom4 = css`
+  padding-bottom: ${spacing.space4};
+`;
+export const paddingBottom5 = css`
+  padding-bottom: ${spacing.space5};
+`;
+
+export const padding2 = css`
+  padding: ${spacing.space2};
+`;
+
+export const paddingY3 = css`
+  padding: ${spacing.space3} 0;
+`;
+
+export const textAlignCenter = css`
+  text-align: center;
+`;
+
+export const fontWeight400 = css`
+  font-weight: 400;
+`;
+
+export const relativeHeightWidth100 = css`
+  position: relative;
+  height: 100%;
+  width: 100%;
+`;
