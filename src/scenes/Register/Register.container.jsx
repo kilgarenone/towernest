9cee8f0c04fb274b58b2512bdb819a5d818b5076
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-26 12:13:11 +0800

    Add input validation for registration

--- src/scenes/Register/Register.container.jsx
+++ src/scenes/Register/Register.container.jsx
@@ -1,12 +1,26 @@
 import { Formik } from "formik";
 import React, { Component } from "react";
 import { css } from "react-emotion";
+import * as Yup from "yup";
 import Button from "../../components/Button";
 import Container from "../../components/Container";
 import FieldInput from "../../components/FieldInput";
 import Heading from "../../components/Heading";
 import { marginRight1, padding3 } from "../../styles/utilities";
 
+const RegisterSchema = Yup.object().shape({
+  first_name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  last_name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  username: Yup.string()
+    .email("Invalid email")
+    .required("Required")
+});
 class Register extends Component {
   state = {};
 
@@ -28,8 +42,9 @@ class Register extends Component {
             <Formik
               initialValues={{ first_name: "", last_name: "", username: "" }}
               enableReinitialize={false}
-              validate={this.validate}
+              validationSchema={RegisterSchema}
               onSubmit={this.handleSubmit}
+              validateOnBlur
             >
               {({ handleSubmit, isSubmitting }) => (
                 <Container tag="form" isColumn onSubmit={handleSubmit}>
