8b916af85f383b8334f0a3ccbc3ea9de153ae519
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-27 21:04:12 +0800

    Refactor root.js

--- src/redux/root.js
+++ src/redux/root.js
@@ -1,11 +1,13 @@
 // @flow
 import { combineEpics } from "redux-observable";
 import { combineReducers } from "redux";
-import userAuth, { getAccessToken } from "../scenes/SignIn/SignIn.state";
+import { getAccessToken } from "../scenes/SignIn/SignIn.state";
+import questionnaire from "../scenes/Questionnaire/Questionnaire.state";
 import api from "./api";
 
 export const rootEpic = combineEpics(api, getAccessToken);
 
 export const rootReducer = combineReducers({
-  userAuth
+  questionnaire
+  // userAuth
 });
