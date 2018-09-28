ad90645a5036e4467d7c792d99095e736361d6f7
Kheoh Yee Wei <kwei88@gmail.com>
2018-09-28 16:47:55 +0800

    Connect PortfolioReview component

--- src/redux/root.js
+++ src/redux/root.js
@@ -3,11 +3,13 @@ import { combineEpics } from "redux-observable";
 import { combineReducers } from "redux";
 import { getAccessToken } from "../scenes/SignIn/SignIn.state";
 import questionnaire from "../scenes/Questionnaire/Questionnaire.state";
+import portfolioReview from "../scenes/PortfolioReview/PortfolioReview.state";
 import api from "./api";
 
 export const rootEpic = combineEpics(api, getAccessToken);
 
 export const rootReducer = combineReducers({
-  questionnaire
+  questionnaire,
+  portfolioReview
   // userAuth
 });
