2f07ecef9603844505f7f58c783e222ccec62020
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-03 18:02:33 +0800

    Refactor <Navbar />

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,23 +11,22 @@ import { colors } from "../styles/colors";
 const HeaderWrapper = styled("header")`
   padding-top: ${spacing.space1};
   padding-bottom: ${spacing.space1};
-  color: ${colors.textOnDarkBg};
+  padding-left: ${spacing.space1};
+  padding-right: ${spacing.space1};
+  box-shadow: inset 0 -1px rgba(0, 0, 0, 0.07);
 `;
 
 const Header = styled("div")`
-  display: flex;
-  justify-content: space-between;
-  padding-left: ${spacing.space1};
-  padding-right: ${spacing.space1};
-  max-width: 1012px;
+  /* max-width: 1012px;
   margin-right: auto;
-  margin-left: auto;
+  margin-left: auto; */
 `;
 
 function NavBar({ children }) {
   return (
     <HeaderWrapper>
-      <Header>{children}</Header>
+      {/* <Header>{children}</Header> */}
+      {children}
     </HeaderWrapper>
   );
 }
