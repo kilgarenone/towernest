528105d64bc3a06e0a842a3137a615647a742097
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-03 18:03:10 +0800

    WIP <Header /> in main app

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { css } from "react-emotion";
-import NavBar from "../components/Navbar";
+import { Link } from "@reach/router";
 import Container from "../components/Container";
-import spacing from "../styles/spacing";
+import Navbar from "../components/Navbar";
+import Logo from "../components/Logo";
+import ExactNavLink from "../components/ExactNavLink";
 
-const Header = () => (
-  <NavBar>
-    <Container yAlign="center">
-      <span
-        className={css`
-          font-weight: 700;
-          font-size: 27px;
-          color: black;
-        `}
-      >
-        Matisa
-      </span>
-      <Container marginLeft={spacing.space5}>
-        <p>hahaha</p>
-      </Container>
-    </Container>
-  </NavBar>
-);
+const linkCss = css`
+  padding: 0 20px;
+`;
+class Header extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Navbar>
+        <Container yAlign="center">
+          <Logo />
+          <Container
+            className={css`
+              margin-left: auto;
+            `}
+          >
+            <ExactNavLink className={linkCss} to="/">
+              Home
+            </ExactNavLink>
+            <ExactNavLink className={linkCss} to="questionnaire">
+              Withdrawal
+            </ExactNavLink>
+            <ExactNavLink className={linkCss} to="plan">
+              Rebalance
+            </ExactNavLink>
+            <ExactNavLink className={linkCss} to="haha">
+              Report
+            </ExactNavLink>
+          </Container>
+          <Container style={{ marginLeft: "auto" }}>
+            <div>My Account</div>
+          </Container>
+        </Container>
+      </Navbar>
+    );
+  }
+}
 
 export default Header;
