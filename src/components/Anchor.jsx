52c3b430c9c6ec7f6c33b9cd94bae40e928b5e6e
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-03 10:59:21 +0800

    Create <Anchor /> component for html <a />

---
+++ src/components/Anchor.jsx
@@ -0,0 +1,12 @@
+import styled from "react-emotion";
+
+const Anchor = styled("a")`
+  color: #304ffe;
+  text-decoration: underline;
+
+  &:hover {
+    color: #1565c0;
+  }
+`;
+
+export default Anchor;
