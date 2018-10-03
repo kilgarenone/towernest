58c3269a3aaa2814db5a2dfcdc1dbeb257c22768
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-03 13:32:09 +0800

    Refine style <ErrorMsg /> with cross icon

--- src/components/ErrorMsg.jsx
+++ src/components/ErrorMsg.jsx
@@ -1,8 +1,11 @@
-import styled from "react-emotion";
+import React from "react";
+import { css, cx } from "react-emotion";
 import { colors } from "../styles/colors";
 import { fontSize } from "../styles/typography";
+import ICONS from "../styles/icons";
+import Icon from "./Icon";
 
-const ErrorMsg = styled("label")`
+const defaultCss = css`
   position: absolute;
   bottom: 0;
   left: 0;
@@ -10,4 +13,21 @@ const ErrorMsg = styled("label")`
   font-size: ${fontSize.subText};
 `;
 
+function ErrorMsg({ className, children, htmlFor, ...props }) {
+  return (
+    <label htmlFor={htmlFor} className={cx(defaultCss, className)} {...props}>
+      <Icon
+        size="20"
+        className={css`
+          background-color: #fbe9e7;
+          border-radius: 9999rem;
+          padding: 0.15em;
+          margin-right: 0.4em;
+        `}
+        icon={ICONS.CLOSE}
+      />
+      <span>{children}</span>
+    </label>
+  );
+}
 export default ErrorMsg;
