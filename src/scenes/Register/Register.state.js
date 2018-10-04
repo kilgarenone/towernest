3b3456c1ec9506d2130b74f6bde08635396d46be
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-04 13:25:27 +0800

    Register client reducer

--- src/scenes/Register/Register.state.js
+++ src/scenes/Register/Register.state.js
@@ -4,18 +4,28 @@ export const REGISTER_CLIENT = "matisa/scenes/Register/registerClient";
 export const STORE_CLIENT = "matisa/scenes/Register/storeClient";
 export const FAIL_REGISTER_CLIENT = "matisa/scenes/Register/failRegisterClient";
 
+export default (state = { clientData: null, error: null }, action) => {
+  switch (action.type) {
+    case STORE_CLIENT:
+      return { ...state, clientData: action.data };
+    case FAIL_REGISTER_CLIENT:
+      return { ...state, error: action.data };
+    default:
+      return state;
+  }
+};
 export function registerClient(payload) {
   return {
     type: CALL_API,
     requestConfig: {
       url: "/registerClient",
       method: POST,
-      body: payload
+      body: payload,
     },
     pluckStateForBody: state => ({
-      metadata: JSON.stringify(state.questionnaire.answers)
+      metadata: JSON.stringify(state.questionnaire.answers),
     }),
     successType: STORE_CLIENT,
-    failureType: FAIL_REGISTER_CLIENT
+    failureType: FAIL_REGISTER_CLIENT,
   };
 }
