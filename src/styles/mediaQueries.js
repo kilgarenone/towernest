8839ce5fda274869ce6d6a5654f9ef67ec32a1f1
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-12 11:59:25 +0800

    convert to em base font size and media queries

--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -4,17 +4,17 @@ import { css } from "react-emotion";
 
 const BREAKPOINTS = {
   // Numerical values will result in a min-width query
-  small: 576,
-  medium: 768,
-  large: 992,
-  xLarge: 1200
+  small: 36,
+  medium: 48,
+  large: 62,
+  xLarge: 75
   // String values will be used as is
   // tallPhone: "(max-width: 360px) and (min-height: 740px)"
 };
 
 const media = Object.keys(BREAKPOINTS).reduce((accumulator, label) => {
   const prefix = typeof BREAKPOINTS[label] === "string" ? "" : "min-width:";
-  const suffix = typeof BREAKPOINTS[label] === "string" ? "" : "px";
+  const suffix = typeof BREAKPOINTS[label] === "string" ? "" : "em";
   accumulator[label] = cls =>
     css`
       @media (${prefix + BREAKPOINTS[label] + suffix}) {
