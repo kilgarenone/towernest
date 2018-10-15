777d6023fed03b06e3db60e467b8a00305b34d83
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-15 16:14:42 +0800

    Adds signup reducer

--- src/redux/root.js
+++ src/redux/root.js
@@ -4,7 +4,7 @@ import { combineReducers } from "redux";
 import { getAccessToken } from "../scenes/SignIn/SignIn.state";
 import questionnaire from "../scenes/Questionnaire/Questionnaire.state";
 import portfolioReview from "../scenes/PortfolioReview/PortfolioReview.state";
-import client from "../scenes/Register/Register.state";
+import signUp from "../scenes/SignUp/SignUp.state";
 import api from "./api";
 
 export const rootEpic = combineEpics(api, getAccessToken);
@@ -12,5 +12,5 @@ export const rootEpic = combineEpics(api, getAccessToken);
 export const rootReducer = combineReducers({
   questionnaire,
   portfolioReview,
-  client,
+  signUp,
 });
