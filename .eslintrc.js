0455dc674b3f33532935463dbceb809ff69f1643
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-25 18:55:00 +0800

    Clean up eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
   extends: [
-    'airbnb',
-    'plugin:flowtype/recommended',
-    'plugin:prettier/recommended',
+    "airbnb",
+    "plugin:flowtype/recommended",
+    "prettier/react",
+    "plugin:prettier/recommended" // this do 3 things 1) extends: 'prettier 2) plugins: 'prettier' 3) rules: prettier/pre: error
   ],
-  plugins: ['flowtype', 'prettier'],
-  parser: 'babel-eslint',
+  plugins: ["flowtype"],
+  parser: "babel-eslint",
   rules: {
-    'prettier/prettier': 'error',
-    'react/prop-types': 'off',
-    'no-use-before-define': 'off',
-    'no-param-reassign': ['error', { props: false }],
-    'react/jsx-no-bind': ['error', { allowArrowFunctions: false }],
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'no-console': ['error', { allow: ['error', 'group', 'groupEnd', 'warn'] }],
-    'react/jsx-indent': 'off',
-    'react/destructuring-assignment': 'off',
-    'jsx-a11y/label-has-for': 'off',
+    "react/prop-types": "off",
+    "no-use-before-define": "off",
+    "no-param-reassign": ["error", { props: false }],
+    "react/jsx-no-bind": ["error", { allowArrowFunctions: false }],
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "no-console": ["error", { allow: ["error", "group", "groupEnd", "warn"] }],
+    "react/jsx-indent": "off",
+    "react/destructuring-assignment": "off",
+    "jsx-a11y/label-has-for": "off"
   },
   env: {
-    browser: true,
+    browser: true
   },
   settings: {
     flowtype: {
-      onlyFilesWithFlowAnnotation: false,
-    },
-  },
+      onlyFilesWithFlowAnnotation: false
+    }
+  }
 };
